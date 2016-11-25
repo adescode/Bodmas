e71ac3b6b3e1131f74a257a0528d57a80062eafc
Adescode <adelaja444@gmail.com>
2016-11-25 22:40:17 +0100

    Infinite looping and some Exceptions rectified

--- src/bodmas/Bodmas.java
+++ src/bodmas/Bodmas.java
@@ -15,124 +15,150 @@ import java.util.Scanner;
  * @author Adescode
  */
 public class Bodmas {
-ArrayList<String> container;
- String temp;
+
+    ArrayList<String> container;
+    String temp;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        String yourExpress;
+        do{
         Scanner input = new Scanner(System.in);
         System.out.println("Enter expression:");
-        String yourExpress=input.nextLine();
+        yourExpress = input.nextLine();
         Bodmas bodmasIs = new Bodmas();
-        String result = bodmasIs.bracket(yourExpress);
-        System.out.println("Answer : " + result);
+        String result = bodmasIs.checkBracket(yourExpress);
+        System.out.println("Answer : " + result
+        +"\t(Enter q to quit)\n");
+        }while(!yourExpress.equals("q"));
+                {
+        if(yourExpress.equals("q")){
+            System.out.println("Done!!!");  
+        }
+    }
     }
     /*Method that reads brackets*/
-    public String bracket(String box){
-        while(box.contains(Character.toString('(')) || box.contains(Character.toString(')'))){ //condition: input with bracket
-            for (int i=0;i<box.length();i++){  
-                try{                    
-                 if((box.charAt(i)==')'|| Character.isDigit(box.charAt(i))) //multiplication oprator to occur 
-                         &&box.charAt(i+1)=='('){                           //after the digit 
-                     box=box.substring(0, i+1)+"*"+box.substring(i+1);      //before open bracket '('
-                 } 
-                }catch(Exception ignored){}                                 //ignore out of bounds exception
-                if(box.charAt(i)==')'){                                            
-                    for(int j=i;j>=0;j--){                                         
-                        if(box.charAt(j)=='('){
-                            String in= box.substring(j+1, i);               //reading method
-                            in= reading(in);                                //should occur
-                            box=box.substring(0, j)+in+box.substring(i+1);  //inside the bracket
-                            j=i=0;
+
+    public String checkBracket(String input) {
+        while (input.contains(Character.toString('(')) || input.contains(Character.toString(')'))) { //condition: input with bracket
+            for (int i = 0; i < input.length(); i++) {
+                try {
+                    if ((input.charAt(i) == ')' || Character.isDigit(input.charAt(i))) //multiplication oprator to occur 
+                            && input.charAt(i + 1) == '(') {                           //after the digit 
+                        input = input.substring(0, i + 1) + "*" + input.substring(i + 1);      //before open bracket '('
+                    }
+                } catch (Exception e) {}                                 //ignore out of bounds exception
+                if (input.charAt(i) == ')') {
+                    for (int j = i; j >= 0; j--) {
+                        if (input.charAt(j) == '(') {
+                            String in = input.substring(j + 1, i);               //reading method
+                            in = read(in);                                //should occur
+                            input = input.substring(0, j) + in + input.substring(i + 1);  //inside the bracket
+                            j = i = 0;
                         }
                     }
                 }
             }
-            if(box.contains(Character.toString('('))||box.contains(Character.toString(')'))){
+            if (input.contains(Character.toString('(')) || input.contains(Character.toString(')'))) {
                 return "Error: incorrect brackets placement";
             }
         }
-        box=reading(box);
-        return box;
+        input = read(input);
+        return input;
     }
     /*Method that reads numbers and operators */
-    public String reading(String read){
-            container = new ArrayList<>();    //keeps input String
-            temp = "";                            //temporary String
-            for(int i=read.length()-1;i>=0;i--){   //precendence order of operation reads from RIGHT to LEFT
-              if(Character.isDigit(read.charAt(i))){
-                  temp = read.charAt(i)+temp;
-                  if(i==0){
-                      if(!temp.equals("")){
-                container.add(0,temp);
-                temp="";
-            }
-                  }
-              }else if(read.charAt(i)=='.' ){                       //reads decimal
-                   temp =read.charAt(i)+temp;
-                  }
-                  else if(read.charAt(i)=='-' && (i==0 ||
-                          (!Character.isDigit(read.charAt(i-1)))))   //read negative numbers
-                  {                                  
-                  temp =read.charAt(i)+temp;
-                  if(!temp.equals("")){
-                container.add(0,temp);
-                temp="";
-            }
-                  }else
-                  {if(!temp.equals("")){
-                container.add(0,temp);
-                temp="";
-            }
-                  temp +=read.charAt(i);
-                  if(!temp.equals("")){
-                container.add(0,temp);
-                temp="";
-            }
-                  }
-            }
-           container = operator(container,"^"); //power
-           container = operator(container,"|"); //root
-           container = operator(container,"/"); //Divide
-           container = operator(container,"*"); //Multiple
-           container = operator(container,"+"); //Add
-           container = operator(container,"-"); //Sub
-           return container.get(0);             // return result
-            }
-    
-    public ArrayList<String>operator(ArrayList<String> oop, String q){
-        int scale=10;                         //to limit decimal places
-        BigDecimal ex = new BigDecimal(0);    //Used instead of any data type
-        for(int c = 0; c<oop.size();c++){
-            if(oop.get(c).equals(q)){
-                switch (oop.get(c)){
-                    case "^":
-                    ex = new BigDecimal(oop.get(c-1)).pow(Integer.parseInt(oop.get(c+1)));
-                    break;
-                    case "|":
-                    ex = new BigDecimal(Math.sqrt(Double.parseDouble(oop.get(c+1))));
-                    break;
-                    case "*":
-                    ex = new BigDecimal(oop.get(c-1)).multiply(new BigDecimal(oop.get(c+1)));
-                    break;
-                    case "/":
-                    ex = new BigDecimal(oop.get(c-1)).divide(new BigDecimal(oop.get(c+1)));
-                    break;
-                    case "+":
-                    ex = new BigDecimal(oop.get(c-1)).add(new BigDecimal(oop.get(c+1)));
-                    break;
-                    case "-":
-                    ex = new BigDecimal(oop.get(c-1)).subtract(new BigDecimal(oop.get(c+1)));
-                    break;
+
+    public String read(String input) {
+        container = new ArrayList<>();    //keeps input String
+        temp = "";                            //temporary String
+        
+        for (int i = input.length() - 1; i >= 0; i--) {   //precendence order of operation reads from RIGHT to LEFT
+            if (Character.isDigit(input.charAt(i))) {
+                temp = input.charAt(i) + temp;
+                if (i == 0) {
+                    check();
                 }
-                oop.set(c, ex.setScale(scale, RoundingMode.UP).stripTrailingZeros().toPlainString());
-                oop.remove(c+1);       //replace operators with results                                                         
-                oop.remove(c-1);        // remove used numbers numbers
-            }else{
-                continue;
-            }c=0;
+            } else if (input.charAt(i) == '.') {                       //reads decimal
+                temp = input.charAt(i) + temp;
+            } else if (input.charAt(i) == '-' && (i == 0
+                    || (!Character.isDigit(input.charAt(i - 1))))) //read negative numbers
+            {
+                temp = input.charAt(i) + temp;
+                check();
+            }else
+                if ((input.charAt(i) == '+'||input.charAt(i) == '*'||
+                        input.charAt(i) == '/'||input.charAt(i) == '^')
+                        && (i == 0 || (!Character.isDigit(input.charAt(i - 1))))){
+                return "Error in input";
+                }
+                
+                else{
+                check();
+                temp += input.charAt(i);
+                check();
+            if(input.charAt(i)=='|'){
+                temp+=" ";
+                check();
+            }
+                }
         }
-        return oop;
+        container = calculate(container, "^"); //power
+        container = calculate(container, "|"); //root
+        container = calculate(container, "/"); //Divide
+        container = calculate(container, "*"); //Multiple
+        container = calculate(container, "+"); //Add
+        container = calculate(container, "-"); //Sub
+        return container.get(0);             // return result
+    }
+    
+    public void check() {
+        if (!temp.equals("")) {
+            container.add(0, temp);
+            temp = "";
+        }
+    }
+    
+    public ArrayList<String> calculate(ArrayList<String> input, String data) {
+        int scale = 10;                         //to limit decimal places
+        BigDecimal ex = new BigDecimal(0);    //Used instead of any data type
+        for (int c = 0; c < input.size(); c++) {
+            if (input.get(c).equals(data)) {
+                switch (input.get(c)) {
+                    case "^":
+                        ex = new BigDecimal(input.get(c - 1)).pow(Integer.parseInt(input.get(c + 1)));
+                        break;
+                    case "|":
+                        ex = new BigDecimal(Math.sqrt(Double.parseDouble(input.get(c + 1))));
+                        break;
+                    case "*":
+                        ex = new BigDecimal(input.get(c - 1)).multiply(new BigDecimal(input.get(c + 1)));
+                        break;
+                    case "/":
+                        try {
+                            ex = new BigDecimal(input.get(c - 1)).divide(new BigDecimal(input.get(c + 1)));
+                            if (input.get(c + 1).equals("0")) {
+                            }
+                        } catch (Exception e) {
+                            System.err.println("Error : " + e.getMessage());
+                        }
+                        break;
+                    case "+":
+                        ex = new BigDecimal(input.get(c - 1)).add(new BigDecimal(input.get(c + 1)));
+                        break;
+                    case "-":
+                        ex = new BigDecimal(input.get(c - 1)).subtract(new BigDecimal(input.get(c + 1)));
+                        break;
+                }
+                input.set(c, ex.setScale(scale, RoundingMode.UP).stripTrailingZeros().toPlainString());
+                input.remove(c + 1);       //replace operators with results                                                         
+                input.remove(c - 1);        // remove used numbers numbers
+            } else {
+                continue;
+            }
+            c = 0;
+        }
+        return input;
     }
 }
